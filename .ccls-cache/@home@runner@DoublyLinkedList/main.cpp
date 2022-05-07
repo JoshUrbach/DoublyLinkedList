e77ce70ce3768aedac3b22c0811e3d658167e655
Josh Urbach <98100405+JoshUrbach@users.noreply.github.com>
2022-05-07 00:05:04 +0000

    Messing with doubly linked list, not a complete program

--- .ccls-cache/@home@runner@DoublyLinkedList/main.cpp
+++ .ccls-cache/@home@runner@DoublyLinkedList/main.cpp
@@ -1,5 +1,70 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Node{
+public:
+  int value;
+  Node* next;
+  Node* previous;
+};
+
+void printForward(Node* head){
+  Node* traverser = head;
+  while(traverser != nullptr){
+    cout << traverser->value << endl; 
+    traverser = traverser->next;
+  }
+}
+
+void printBackward(Node* tail){
+  Node* traverser = tail;
+  while(traverser != nullptr){
+    cout << traverser->value << endl; 
+    traverser = traverser->previous;
+  }
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  Node* head;
+  Node* tail;
+
+  //add first node
+  Node* node = new Node;
+  node->value = 4;
+  node->next = nullptr;
+  node->previous = nullptr;
+  head = node;
+  tail = node;
+
+  //add second node
+  node = new Node();
+  node->value = 5;
+  node->next = nullptr;
+  node->previous = tail;
+  tail->next = node;
+  tail = node;
+
+  //add third node
+  node = new Node();
+  node->value = 6;
+  node->next = nullptr;
+  node->previous = tail;
+  tail->next = node;
+  tail = node;
+
+  //add fourth node
+  node = new Node();
+  node->value = 7;
+  node->next = nullptr;
+  node->previous = tail;
+  tail->next = node;
+  tail = node;
+
+  printForward(head);
+  printBackward(tail);
+  
+  cin.get();
 }
